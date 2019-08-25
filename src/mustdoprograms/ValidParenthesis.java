1b038574e2badd3330cb513a0f6bc2020caa3e43
Supriya Mishra <supriyamishra05992@gmail.com>
2019-08-25 23:58:44 +0530

    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.

---
+++ src/mustdoprograms/ValidParenthesis.java
@@ -0,0 +1,18 @@
+package mustdoprograms;
+
+/***
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ *
+ * Example 1:
+ *
+ * Input: "()"
+ * Output: true
+ */
+public class ValidParenthesis {
+}
