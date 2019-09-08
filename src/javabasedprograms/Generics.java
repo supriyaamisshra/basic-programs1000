79e869a1bb1fd7fc0f864d6ea0246a1be2abb7c0
Supriya Mishra <supriyamishra05992@gmail.com>
2019-09-09 03:38:23 +0530

    java generics  hackerrank

---
+++ src/javabasedprograms/Generics.java
@@ -0,0 +1,31 @@
+package javabasedprograms;
+
+/***
+ * an integer array and a string array. You have to write a single method printArray that can print all the elements of both arrays. The method should be able to accept both integer arrays or string arrays.
+ *
+ * You are given code in the editor. Complete the code so that it prints the following lines:
+ *
+ * 1
+ * 2
+ * 3
+ * Hello
+ * World
+ * Do not use method overloading because your answer will not be accepted.
+ */
+public class Generics {
+
+
+    private static void printArray(Object[] input) {
+        for (Object o: input) {
+            System.out.println(o);
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] number = {1,22,3};
+        String[] str = {"Hello", "Supriya"};
+        printArray(number);
+        printArray(str);
+
+    }
+}
